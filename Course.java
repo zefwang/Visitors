077ea5a968949970c8992e09a89a9f0a6a1f2027
Zefeng (Daniel) Wang <wang.zef@husky.neu.edu>
2019-02-24 19:26:42 -0500

    Added the HasPrereq method and all the tests.

--- Course.java
+++ Course.java
@@ -1,3 +1,5 @@
+import tester.Tester;
+
 interface IList<T> {
   <R> R accept(IListVisitor<T, R> visitor);
 }
@@ -36,7 +38,7 @@ class Course {
   }
 
   boolean hasPrereq(String target) {
-    return this.prereqs.accept(new HasPrereq(target));
+    return new HasPrereq(target).apply(this);
     // .hasClass(target);
   }
 
@@ -70,29 +72,67 @@ class DeepestPathLength implements IListVisitor<Course, Integer> {
   }
 }
 
-interface IPred<X> extends IFunc<IList<Course>, Boolean> {
-  Boolean apply(IList<Course> prereqs);
+interface IPred<X> extends IFunc<X, Boolean> {
 }
 
-class HasPrereq implements IPred<Course>, IListVisitor<Course, Boolean> {
+class HasPrereq implements IPred<Course> {
   String target;
 
   HasPrereq(String target) {
     this.target = target;
   }
 
-  public Boolean apply(IList<Course> arg) {
-    return false;
-  }
-
-  public Boolean forMt(MtList<Course> arg) {
-    return true;
-  }
-
-  public Boolean forCons(ConsList<Course> arg) {
-    return false;
+  public Boolean apply(Course arg) {
+    return new HasReqHelper(target).apply(arg.prereqs);
   }
 }
 
+class HasReqHelper implements IListVisitor<Course, Boolean> {
+  String target;
+
+  HasReqHelper(String target) {
+    this.target = target;
+  }
+
+  public Boolean apply(IList<Course> arg) {
+    return arg.accept(this);
+  }
+
+  public Boolean forMt(MtList<Course> arg) {
+    return false;
+  }
+
+  public Boolean forCons(ConsList<Course> arg) {
+    return arg.first.hasName(target) || arg.rest.accept(this);
+  }
+
+}
+
 class ExamplesCourses {
+  Course c1 = new Course("CS 1", new MtList<Course>());
+  Course c2 = new Course("CS 2", new MtList<Course>());
+  IList<Course> cList1And2 = new ConsList<Course>(c2,
+      new ConsList<Course>(c1, new MtList<Course>()));
+  Course c3 = new Course("CS 3", cList1And2);
+  Course c4 = new Course("CS 4", new MtList<Course>());
+  Course c5 = new Course("CS 5", new MtList<Course>());
+  IList<Course> cList34And5 = new ConsList<Course>(c3,
+      new ConsList<Course>(c4, new ConsList<Course>(c5, new MtList<Course>())));
+  Course c6 = new Course("CS 6", cList34And5);
+  Course c7 = new Course("CS 7", new ConsList<Course>(c6, new MtList<Course>()));
+
+  boolean testDeepestPathLength(Tester t) {
+    return t.checkExpect(c1.getDeepestPathLength(), 0)
+        && t.checkExpect(c3.getDeepestPathLength(), 1)
+        && t.checkExpect(c6.getDeepestPathLength(), 2)
+        && t.checkExpect(c4.getDeepestPathLength(), 0)
+        && t.checkExpect(c7.getDeepestPathLength(), 3);
+  }
+
+  boolean testHasPrereq(Tester t) {
+    return t.checkExpect(c1.hasPrereq("CS 3"), false) && t.checkExpect(c3.hasPrereq("CS 1"), true)
+        && t.checkExpect(c3.hasPrereq("CS 4"), false) && t.checkExpect(c4.hasPrereq("CS 1"), false)
+        && t.checkExpect(c6.hasPrereq("CS 1"), true) && t.checkExpect(c7.hasPrereq("CS 5"), true)
+        && t.checkExpect(c7.hasPrereq("CS 2"), true) && t.checkExpect(c7.hasPrereq("CS 0"), false);
+  }
 }
